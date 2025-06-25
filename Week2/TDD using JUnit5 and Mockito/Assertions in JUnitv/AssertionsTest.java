d4e55619e95c31b437b7ce4790df6db12622f3ea
Aditi23k <aditikushwaha2303@gmail.com>
2025-06-25 22:42:20 +0530

    Add files via upload

---
+++ Week2/TDD using JUnit5 and Mockito/Assertions in JUnitv/AssertionsTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.testng.annotations.Test;          // Import @Test annotation
+
+import static org.testng.AssertJUnit.*;
+
+public class AssertionsTest {
+
+        @Test
+        public void testAssertions() {
+            // Assert equals
+            assertEquals(5, 2 + 3);
+
+            // Assert true
+            assertTrue(5 > 3);
+
+            // Assert false
+            assertFalse(5 < 3);
+
+            // Assert null
+            assertNull(null);
+
+            // Assert not null
+            assertNotNull(new Object());
+        }
+    }
+
